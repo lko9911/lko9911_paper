7f86078ff5b58470048f766e8f23db83fc64f335
lko9911 <160494158+lko9911@users.noreply.github.com>
2024-11-16 11:07:00 +0900

    Update and rename YOLO(객체감지).py to YOLOv10x_사용코드.py

--- YOLOv10x_사용코드.py
+++ YOLOv10x_사용코드.py
@@ -3,14 +3,16 @@ from ultralytics import YOLO
 import cv2
 import torch
 
+# GPU 사용 확인 필수_1
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 img_path = "content/test/images/val_image (251).png"
 
-# YOLO 모델 로드
+# YOLO 모델 로드 (재학습 안하고 그대로 사용 (v10 버전))
 yolo_model = YOLO("yolov10x.pt")
 
 # YOLO 모델로 객체 탐지 (신뢰도 조정)
 results = yolo_model.predict(img_path, imgsz=640, conf=0.5, save=False, show=True)
 
+# 윈도우 필수_2
 cv2.waitKey(0)
 cv2.destroyAllWindows()
